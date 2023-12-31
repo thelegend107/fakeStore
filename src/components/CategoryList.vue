151490b7e368c050b7e6d11fe079e90ef70e8f7b
Moe Ayoub <mma.ayoub@outlook.com>
2023-12-31 09:02:49 -0600

    Initial Development 1.2.1
    Fixing up categories for mobile.

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
+
 const props = defineProps({
     mainCategory: Object,
     categories: Array
 })
 
 const emit = defineEmits(['mainCategoryChange'])
+
+// Mobile version of categories
+const categoryIndex = ref(0);
+const categorySize = props.categories.length;
+function handleCategoryNavigation(num) {
+    if (categoryIndex.value == 0 && num < 0)
+        categoryIndex.value = categorySize - 1;
+    else if (categoryIndex.value == categorySize - 1 && num > 0)
+        categoryIndex.value = 0;
+    else if (num < 0)
+        categoryIndex.value--;
+    else
+        categoryIndex.value++;
+
+    emit('mainCategoryChange', categoryIndex.value);
+}
 </script>
 
 <template>
@@ -13,12 +33,49 @@ const emit = defineEmits(['mainCategoryChange'])
             {{ c.name }}
         </h3>
     </div>
+    <div class="categories-mobile">
+        <button @click="handleCategoryNavigation(-1)"><SvgIcon type="mdi" :path="mdiChevronLeft" /></button>
+        <h3 class="category" @click="emit('mainCategoryChange', categoryIndex)">{{ props.categories[categoryIndex].name }}</h3>
+        <button @click="handleCategoryNavigation(1)"><SvgIcon type="mdi" :path="mdiChevronRight" /></button>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 .categories {
+    display: none;
+}
+
+.categories-mobile {
     width: 100%;
-    background-color: var(--primary);
+    background-color: var(--primaryV);
+    display: flex;
+    justify-content: space-between;
+    text-align: center;
+    align-items: center;
+    overflow: hidden;
+    gap: 3vw;
+    color: white;
+    padding: 0 3vw;
+
+    .category {
+        padding: 0.5rem;
+        transition: all 0.3s ease;
+    }
+}
+
+.categoryActive {
+    color: white;
+    border-bottom: 2px solid white;
+}
+
+@media (min-width: 1024px) and (min-height: 788px) {
+    .categories-mobile {
+        display: none;
+    }
+
+    .categories {
+    width: 100%;
+    background-color: var(--primaryV);
     display: flex;
     justify-content: center;
     align-items: center;
@@ -36,9 +93,5 @@ const emit = defineEmits(['mainCategoryChange'])
         border-bottom: 2px solid white;
     }
 }
-
-.categoryActive {
-    color: white;
-    border-bottom: 2px solid white;
 }
 </style>
